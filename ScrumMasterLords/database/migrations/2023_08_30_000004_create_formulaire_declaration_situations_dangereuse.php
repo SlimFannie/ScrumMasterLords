6c8b4a5827042eb7344f8eba3ca00cdd896407ff
fchalifour <93669813+Infinitrix10018@users.noreply.github.com>
2023-09-01 12:48:44 -0400

    modification nom d'une miggration Laravel

--- ScrumMasterLords/database/migrations/2023_08_30_000004_create_formulaire_declaration_situations_dangereuse.php
+++ ScrumMasterLords/database/migrations/2023_08_30_000004_create_formulaire_declaration_situations_dangereuse.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('formulaire_declaration_accidents', function (Blueprint $table) {
+        Schema::create('formulaire_declaration_situation_dangereuses', function (Blueprint $table) {
             $table->id()->autoIncrement();
             $table->string('nom_eployer')->nullable();
             $table->string('fonction')->nullable();
@@ -42,6 +42,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('formulaire_declaration_accidents');
+        Schema::dropIfExists('formulaire_declaration_situation_dangereuses');
     }
 };
