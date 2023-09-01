3cfb463795d61cab275077de5708e07a92256639
Grondin Alexandre <117280087+Miro974@users.noreply.github.com>
2023-09-01 14:32:12 -0400

    Debut form

--- ScrumMasterLords/resources/views/formulaires/formDanger.blade.php
+++ ScrumMasterLords/resources/views/formulaires/formDanger.blade.php
@@ -1,1 +1,50 @@
-<h1> YOOOOO!! <h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="card">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <p>LOGO<p>
+            </div>
+            <div class="col text-center">
+                <h5>Mes formulaires<h5>
+            </div>
+            <div class="col">
+                <p>PHOTO<p>
+            </div>
+        </div>
+            <div class="row mt-4">
+                <div class="col text-center">
+                    <h3>Signalement d'une situation dangereuse, d'un acte de violence ou d'un "passé proche"<h3>
+                </div>
+            </div>
+    </div>
+</div>
+
+<div class="container-fluid">
+    <div class="form">
+        <div class="card">
+            
+        </div>
+    </div>
+</div>
+    
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+
+
+
+</html>
+
