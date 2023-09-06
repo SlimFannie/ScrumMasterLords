cca04cced327292856fc60b5734a5ac82badabb8
Grondin Alexandre <117280087+Miro974@users.noreply.github.com>
2023-09-06 11:12:18 -0400

    Début responsive formulaire situation dangereuse

--- ScrumMasterLords/resources/views/formulaires/formDanger.blade.php
+++ ScrumMasterLords/resources/views/formulaires/formDanger.blade.php
@@ -1,50 +1,95 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Document</title>
-</head>
-<body>
+@extends('layout')
+@section('contenu')
 
-<div class="card">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col">
-                <p>LOGO<p>
-            </div>
-            <div class="col text-center">
-                <h5>Mes formulaires<h5>
-            </div>
-            <div class="col">
-                <p>PHOTO<p>
-            </div>
-        </div>
-            <div class="row mt-4">
-                <div class="col text-center">
-                    <h3>Signalement d'une situation dangereuse, d'un acte de violence ou d'un "passé proche"<h3>
+<div class="container-fluid g-0">
+
+    <div class="container-fluid g-0">
+        <div class="card">   
+            <div class="row g-0">
+                <div class="col-4  text-center g-0">
+                    <p>LOGO<p>
+                </div>
+                <div class="col-4  text-center g-0">
+                    <h5>Mes formulaires<h5>
+                </div>
+                <div class="col-4  text-center g-0">
+                    <p>PHOTO<p>
                 </div>
             </div>
-    </div>
-</div>
-
-<div class="container-fluid">
-    <div class="form">
-        <div class="card">
-            
         </div>
     </div>
+
+    <div class="container-fluid g-0">
+        <div class="card mt-3 g-0" style="width: 750px" id="titre1">
+            <div class="row g-0">
+                <div class="col-10 offset-col-3 text-center m-4 g-0">
+                    <h3 id="titre2">Signalement d'une situation dangereuse, d'un acte de violence ou d'un "passé proche"<h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid g-0">
+        <div class="card mt-3 g-0">
+            <div class="row g-0">
+                    <div class="col text-center m-2 g-0">
+                        <h3>Identification<h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+
+    <div class="container-fluid g-0">
+        <form>
+            <div class="row g-0">
+                <div class="col-lg-3 offset-lg-2 col-6 mt-5 g-0">
+                    <header for="nom">Nom</header>
+                    <input id="nom">
+                </div>
+                <div class="col-lg-3 col-6 mt-5 g-0">
+                    <header for="prenom">Prénom</header>
+                    <input id="prenom">
+                </div>
+
+                <div class="col-lg-3 col-12 mt-5 g-0">
+                    <header for="matricule">Matricule</header>
+                    <input id="matricule">
+                </div>
+            </div>
+            <div class="row g-0">
+                <div class="col-lg-3 offset-lg-2 col-6 mt-5 g-0">
+                    <header for="fonction">Fonction au moment de l'évènement</header>
+                    <input id="fonction" style="width: 270px">
+                </div>
+                <div class="col-lg-4 col-6 mt-5 g-0">
+                    <header for="secteur">Secteur d'activité</header>
+                    <input id="secteur">
+                </div>
+            </div>
+           
+    </div>
+
+    <div class="container-fluid g-0">
+        <div class="card mt-5 g-0">
+            <div class="row g-0">
+                <div class="col text-center m-4 g-0">
+                    <h3>Description de la situation ou du "passé proche"<h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </form>
+
 </div>
-    
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</body>
+@endsection
 
 
 
 
-</html>
+
+
+
+
 
