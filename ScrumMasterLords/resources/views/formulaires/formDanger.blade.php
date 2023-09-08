b2f903a9b998b1440a33991bd447f84c15272cbf
Grondin Alexandre <117280087+Miro974@users.noreply.github.com>
2023-09-08 14:43:34 -0400

    Css form situation dangereuse

--- ScrumMasterLords/resources/views/formulaires/formDanger.blade.php
+++ ScrumMasterLords/resources/views/formulaires/formDanger.blade.php
@@ -3,67 +3,86 @@
 
 <div class="container-fluid g-0">
 
-    <div class="container-fluid g-0" id="titre1">
-        <div class="card mt-3 g-0" style="width: 750px" >
+    <div class="container-fluid g-0" id="sign">
+        <div class="mt-3 g-0" id="bgtitre1" >
             <div class="row g-0">
                 <div class="col-10 offset-col-3 text-center m-4 g-0">
-                    <h3 id="titre2">Signalement d'une situation dangereuse, d'un acte de violence ou d'un "passé proche"<h3>
+                    <h3>Signalement d'une situation dangereuse, d'un acte de violence ou d'un "passé proche"<h3>
                 </div>
             </div>
         </div>
     </div>
 
     <div class="container-fluid g-0">
-        <div class="card mt-3 g-0">
+        <div class="mt-3 g-0" id="bgtitre2">
             <div class="row g-0">
                     <div class="col text-center m-2 g-0">
-                        <h3>Identification<h3>
+                        <h3 id="titre2">Identification<h3>
                     </div>
                 </div>
             </div>
         </div>
     </div>    
 
-    <div class="container-fluid g-0">
+    <div class="container-fluid g-0" id="bgID">
         <form>
             <div class="row g-0">
-                <div class="col-lg-3 offset-lg-2 col-6 mt-5 g-0">
-                    <header for="nom">Nom</header>
-                    <input id="nom">
+                <div class="col-lg-3 offset-lg-2 col-6 mt-3 g-0">
+                    <header for="nom" id="header">Nom</header>
+                    <input id="nom" class="champ">
                 </div>
-                <div class="col-lg-3 col-6 mt-5 g-0">
-                    <header for="prenom">Prénom</header>
-                    <input id="prenom">
+                <div class="col-lg-3 col-6 mt-3 g-0">
+                    <header for="prenom" id="header">Prénom</header>
+                    <input id="prenom" class="champ">
                 </div>
 
                 <div class="col-lg-3 col-12 mt-5 g-0">
-                    <header for="matricule">Matricule</header>
-                    <input id="matricule">
+                    <header for="matricule" id="header">Matricule</header>
+                    <input id="matricule" class="champ">
                 </div>
             </div>
             <div class="row g-0">
                 <div class="col-lg-3 offset-lg-2 col-6 mt-5 g-0">
-                    <header for="fonction">Fonction au moment de l'évènement</header>
-                    <input id="fonction" style="width: 270px">
+                    <header for="fonction" id="header">Fonction au moment de l'évènement</header>
+                    <input id="fonction" class="champ">
                 </div>
-                <div class="col-lg-4 col-6 mt-5 g-0">
-                    <header for="secteur">Secteur d'activité</header>
-                    <input id="secteur">
+                <div class="col-lg-4 col-6 g-0" id="divSecteur">
+                    <header for="secteur" id="header">Secteur d'activité</header>
+                    <input id="secteur" class="champ">
                 </div>
             </div>
            
     </div>
 
     <div class="container-fluid g-0">
-        <div class="card mt-5 g-0">
+        <div class="g-0" id="bgtitre2">
             <div class="row g-0">
                 <div class="col text-center m-4 g-0">
-                    <h3>Description de la situation ou du "passé proche"<h3>
+                    <h3 id="titre2">Description de la situation ou du "passé proche"<h3>
                 </div>
             </div>
         </div>
     </div>
 
+    <div class="container-fluid g-0" id="description">
+        <div class="row g-0">
+            <div class="col g-0">
+                <header for="dateHeure" id="header">Date et heure de l'observation :</header>
+                <input type="datetime-local" id="dateHeure" class="champ">
+            </div>
+            <div class="col g-0" id="divLieu">
+                <header for="lieu" id="header">Lieu</header>
+                <input id="lieu" class="champ">
+            </div>
+        </div>
+
+        <div class="row" id="iEvent">
+            <div class="col text-center">
+                <header for="dEvent" id="header">Description :</header>
+                <textarea id="dEvent"></textarea>
+            </div>
+        </div>
+    </div>
     </form>
 
 </div>
