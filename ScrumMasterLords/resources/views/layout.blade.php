21532ef2c00432198fdc3309862b1c78649cd3d8
Fannie Hamel Thibault <90933505+SlimFannie@users.noreply.github.com>
2023-09-13 08:03:28 -0400

    small push

--- ScrumMasterLords/resources/views/layout.blade.php
+++ ScrumMasterLords/resources/views/layout.blade.php
@@ -21,7 +21,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mb-2 mb-lg-0 mx-auto">
         <li class="nav-item">
-          <a class="nav-link nav-link-lg" href="#">Nouveau formulaire <i class="fa-solid fa-pen-to-square"></i></a>
+          <button class="nav-link nav-link-lg noBtn" type="button" data-bs-toggle="collapse" data-bs-target="#sideBar" aria-controls="sideBar" aria-expanded="false" aria-label="Toggle navigation">Nouveau formulaire <i class="fa-solid fa-pen-to-square"></i></a>
         </li>
         <li class="nav-item">
           <a class="nav-link nav-link-lg" href="#">Mes formulaires <i class="fa-solid fa-list-check"></i></a>
@@ -31,6 +31,12 @@
     </div>
   </div>
 </nav>
+<div class="collapse" id="sideBar">
+  <div class="bg-light">
+    <h5 class="h4">Collapsed content</h5>
+    <span class="text-muted">Toggleable via the navbar brand.</span>
+  </div>
+</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 <script src="https://kit.fontawesome.com/59d2871224.js" crossorigin="anonymous"></script>
 </body>
