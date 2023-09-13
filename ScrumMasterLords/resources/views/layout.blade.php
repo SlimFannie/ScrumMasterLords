f84b0d95777aa04ba4556ad31134d22a246ed341
Fannie Hamel Thibault <90933505+SlimFannie@users.noreply.github.com>
2023-09-13 10:45:27 -0400

    Navbar

--- ScrumMasterLords/resources/views/layout.blade.php
+++ ScrumMasterLords/resources/views/layout.blade.php
@@ -10,29 +10,56 @@
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid px-5">
-    <a class="navbar-brand" href="#">
-      <img src="/img/logo_v3r_n_et_blanc.jpg" alt="Ville de Trois-Rivières" height="80px" width="70px">
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mb-2 mb-lg-0 mx-auto">
-        <li class="nav-item">
-          <a class="nav-link nav-link-lg" href="#">Nouveau formulaire <i class="fa-solid fa-pen-to-square"></i></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link nav-link-lg" href="#">Mes formulaires <i class="fa-solid fa-list-check"></i></a>
-        </li>
-      </ul>
-      <img src="/img/saladeProfil.jpg" alt="Ville de Trois-Rivières" class="rounded-3" height="80px" width="80px">
-    </div>
-  </div>
-</nav>
-
-@yield('contenu')
+<div class="container-fluid g-0 h-100 fixed-top">
+  <nav class="navbar navbar-expand-lg bg-light h-15">
+      <div class="row text-center d-flex align-items-center px-5 w-100">
+        <div class="col-2">
+          <a class="navbar-brand " href="#">
+            <img src="/img/logo_v3r_n_et_blanc.jpg" class="d-inline-flex" alt="Ville de Trois-Rivières" height="80px" width="70px">
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+        <div class="col-8">
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mb-2 mb-lg-0 mx-auto">
+              <li class="nav-item">
+                <button class="nav-link nav-link-lg noBtn" href="#" type="button" data-bs-toggle="collapse" data-bs-target="#newForm" aria-controls="newForm" aria-expanded="false" aria-label="Toggle navigation">Nouveau formulaire <i class="fa-solid fa-pen-to-square"></i></button>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link nav-link-lg" href="#">Mes formulaires <i class="fa-solid fa-list-check"></i></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-2">
+          <img src="/img/saladeProfil.jpg" alt="Ville de Trois-Rivières" class="rounded-3" height="80px" width="80px">
+        </div>
+      </div>
+  </nav>
+      <div class="row d-flex align-items-center text-center h-85 g-0">
+        <div class="col-12">
+          @yield('contenu')
+          <div class="collapse collapse-horizontal" id="newForm">
+            <ul class="noBullet">
+              <li>
+                Formulaire SST
+              </li>
+              <li>
+                Situation dangereuse
+              </li>
+              <li>
+                Audit SST
+              </li>
+              <li>
+                Atelier mécanique
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 <script src="https://kit.fontawesome.com/59d2871224.js" crossorigin="anonymous"></script>
