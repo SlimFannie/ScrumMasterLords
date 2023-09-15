c86af480e2fd82f4692263e559e91664a8b3055c
Fannie Hamel Thibault <90933505+SlimFannie@users.noreply.github.com>
2023-09-15 14:30:42 -0400

    vendredi 15 septembre
    script.js online

---
+++ ScrumMasterLords/public/js/script.js
@@ -0,0 +1,9 @@
+var x = document.getElementById("app");
+
+function showNav() {
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
