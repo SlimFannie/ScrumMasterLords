dc80b8ff0429e52f1ffb40d178c7d26e66f7dc21
Alexandre Carle <117288951+SsBarbatos@users.noreply.github.com>
2023-09-20 10:44:03 -0400

    css

--- ScrumMasterLords/app/Http/Controllers/DeclarationAccidentsController.php
+++ ScrumMasterLords/app/Http/Controllers/DeclarationAccidentsController.php
@@ -12,7 +12,7 @@ class DeclarationAccidentsController extends Controller
     // les formulaires des ses employers
     public function index()
     {
-        DB::select('select id, nom_formulaire, nom_employer, date_accident where matricule_usager = ? or ? = ANY (select matricule_usager where ');
+        // DB::select('select id, nom_formulaire, nom_employer, date_accident where matricule_usager = ? or ? = ANY (select matricule_usager where ');
         return View('formulaires.formAccidentTravail');
     }
 
--- ScrumMasterLords/resources/views/layout.blade.php
+++ ScrumMasterLords/resources/views/layout.blade.php
@@ -8,6 +8,9 @@
     <!-- CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/styleAT.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/styleSST.css') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
 </head>
 <body>
 <div class="container-fluid g-0 h-100">
@@ -38,7 +41,7 @@
         </div>
       </div>
   </nav>
-      <div class="row d-flex align-items-center text-center h-85 g-0">
+      <div class="row h-85 g-0">
         <div class="col-12">
           <div class="bgApp" id="app">@yield('contenu')</div>
           <div class="collapse collapse-horizontal bgNav" id="newForm">
@@ -61,8 +64,8 @@
       </div>
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-<script src="https://kit.fontawesome.com/59d2871224.js" crossorigin="anonymous"></script>
-<script src="{{ asset('js/script.js') }}" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/59d2871224.js" crossorigin="anonymous"></script>
+  <script src="{{ asset('js/script.js') }}" crossorigin="anonymous"></script>
 </body>
 </html>
