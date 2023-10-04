e370cddef67cd3187b92f84515c9450c4fd2699f
Alexandre Carle <117288951+SsBarbatos@users.noreply.github.com>
2023-10-04 09:01:00 -0400

    test

--- ScrumMasterLords/resources/views/layout.blade.php
+++ ScrumMasterLords/resources/views/layout.blade.php
@@ -42,6 +42,9 @@
       </div>
   </nav>
   
+  @yield('contenu')
+
+    <!--
       <div class="row g-0 h-85">
         <div class="col-12 h-100">
           <div class="bgApp collapse collapse-horizontal show h-100" id="newForm">@yield('contenu')</div>
@@ -63,6 +66,7 @@
           </div>
         </div>
       </div>
+-->
 </div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
