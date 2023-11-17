60cfbf9bec3c38273e6f61f2d67ca9acb8686aea
Fannie Hamel Thibault <90933505+SlimFannie@users.noreply.github.com>
2023-11-17 10:09:08 -0500

    backend2

--- ScrumMasterLords/app/Http/Controllers/FormulairesController.php
+++ ScrumMasterLords/app/Http/Controllers/FormulairesController.php
@@ -1,64 +1,54 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Usager;
-use App\Models\Mecanique;
-use App\Models\Accident;
+use App\Models\Formulaire;
 use Illuminate\View\View;
 use Illuminate\Auth\Middleware;
-use Auth;
 use DB;
 use Session;
 use Validator;
 
 class FormulairesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        return View('welcome')->with('username', self::getUsername());
+    
+    public function index() {
+        $usager = Usager::find(Session::get('user.id'));
+        $formulaires = $usager->formulaires;
+
+
+        return View('welcome', ['formulaires'=>$formulaires]);
     }
 
-    /* Accident de travail */
+    // Accident de travail
 
     public function accident() {
-        return View('formulaires.formAccidentTravail')->with('username', self::getUsername());
+        $superieurs = Usager::where('superieur', true)->get();
+        return view('formulaires.accident', ['username'=>Session::get('username'), 'superieurs'=>$superieurs]);
     }
 
-    public function storeAccident(Request $request) {
-        try {
-            $accident = new Accident($request->all());
-            $accident->save();
-            return View('welcome')->with('username', self::getUsername());
-        } catch (\Throwable $e) {
-            Log::debug($e);
+    public function storeAccident(Request $request): RedirectResponse {
+        $validator = Validator::make($request->all(), [
+            'fonction' => ['required'],
+            'dateHeure' => ['required'],
+            'endroit' => ['required'],
+            'endroitBlessure' => ['required'],
+            'descriptionBlessure' => ['required'],
+            'histoire' => ['required'],
+            'superieur' => ['required']
+        ]);
+
+        if($validator->fails()) {
+            return redirect()->route('formulaires.accident', ['username'=>Session::get('username')])->withErrors($validator)->withInput();
+        } else {
+            return redirect()->route('dashboard', ['username'=>Session::get('username')]);
         }
-    }
-
-    public function showAccident() {
 
     }
-    
-    public function audit() {
-
-        return View('formulaires.formAuditSST')->with('username', self::getUsername());
-    }
-
-    public function atelier() {
-        return View('formulaires.formAtelierMecanique')->with('username', self::getUsername());
-    }
-
-    public function danger() {
-        return View('formulaires.formDanger')->with('username', self::getUsername());
-    }
-
-    public function getUsername() {
-        return $username = Session::get('prenom').Session::get('nom');
-    }
-
 }
